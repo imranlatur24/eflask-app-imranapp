ee986d1f7762327864b77e0590cd963be60585f6
imran <imranlatur33@gmail.com>
2020-06-10 18:17:49 +0530

    make it better

--- app/main.py
+++ app/main.py
@@ -21,7 +21,18 @@ def webhook():
     print("Hallo world")
     #matching action value
 
-
+def getweather(cityname):
+    api_address='https://api.openweathermap.org/data/2.5/weather?appid=3e90723e72e3e77055f8c10dccb120f8&q='
+                # city = input('City Name :')
+    city = cityname
+    print("city :",city)
+    url = api_address + city
+    print("url :",url)
+    json_data = requests.get(url).json()
+    print("json_data :",json_data)
+    formatted_data = json_data['main']['temp']
+    print("formatted_data",formatted_data)
+    return formatted_data
        
 
     if action == "hi":
@@ -68,17 +79,15 @@ def webhook():
     elif action == "city":
            #if 'username' in request.args:
             #return 'Hello ' + request.args['name']
-            def getweather(cityname):
-                api_address='https://api.openweathermap.org/data/2.5/weather?appid=3e90723e72e3e77055f8c10dccb120f8&q='
-                # city = input('City Name :')
-                city = cityname
-                print("city :",city)
-                url = api_address + city
-                print("url :",url)
-                json_data = requests.get(url).json()
-                print("json_data :",json_data)
-                formatted_data = json_data['main']['temp']
-                print("formatted_data",formatted_data)
+            api_address='https://api.openweathermap.org/data/2.5/weather?appid=3e90723e72e3e77055f8c10dccb120f8&q='
+            city = cityname
+            print("city :",city)
+            url = api_address + city
+            print("url :",url)
+            json_data = requests.get(url).json()
+            print("json_data :",json_data)
+            formatted_data = json_data['main']['temp']
+            print("you city max temp is :",formatted_data)
             return formatted_data
 
     elif action == "errorresponse":
--- app/weather.py
+++ app/weather.py
@@ -1,51 +1,16 @@
 from flask import *
-import sqlite3
 import json
+import requests
 
-app = Flask(__name__)
+cityname = input("Enter your cityname : ") 
 
-@app.route('/webhook', methods=['POST'])
-def webhook():
-    req = request.get_json(silent=True, force=True)
-    #try:
-    action = req.get('queryResult').get('action')
-    cityname = req.get('queryResult').get('queryText')
-    not_yet = req.get('queryResult').get('queryText')
-       
-       #name = req.get('parameters').get('name.original')
-    print('this is name of user '+not_yet)
-       
-    #except AttributeError:
-    #    return 'errro by json'
-    print(req)
-    print("Hallo world")
-    #matching action value
-
-     
-       https://api.openweathermap.org/data/2.5/weather?appid=3e90723e72e3e77055f8c10dccb120f8&q=
-
-    if action == "hi":
-            return make_response(jsonify({'fulfillmentText': "I h've headphones",
-                 "fulfillmentMessages":[
-      {"platform":"PLATFORM_UNSPECIFIED","text":{"text":["enter your city name.."],"mesasage":"text"},
-           ] }))
-
-
-   
-    elif action == "city":
-           #if 'username' in request.args:
-            #return 'Hello ' + request.args['name']
-            cityname = req.get('queryResult').get('queryText')
-            api_address='https://api.openweathermap.org/data/2.5/weather?appid=3e90723e72e3e77055f8c10dccb120f8&q='
-            # city = input('City Name :')
-            city = cityname
-            url = api_address + city
-            json_data = requests.get(url).json()
-            formatted_data = json_data['main']['temp']
-            print(formatted_data)
-            return make_response(jsonify({'fulfillmentText': "your city tempreture is: ",formatted_data))
-        
-
-@app.route("/") 
-def home_view(): 
-		return "<h1>Welcome 2 Imran Server</h1>"
+    
+api_address='https://api.openweathermap.org/data/2.5/weather?appid=3e90723e72e3e77055f8c10dccb120f8&q='
+city = cityname
+print("city :",city)
+url = api_address + city
+print("url :",url)
+json_data = requests.get(url).json()
+print("json_data :",json_data)
+formatted_data = json_data['main']['temp']
+print("you city temp is :",formatted_data)

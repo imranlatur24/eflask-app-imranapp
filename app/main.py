8458ee73f0c34d609f5ff95120c9bfffffef585f
imran <imranlatur33@gmail.com>
2020-06-10 13:50:57 +0530

    make it better

--- app/main.py
+++ app/main.py
@@ -55,15 +55,8 @@ def webhook():
     if action == "notyet":
            #if 'username'  request.args:
             #return 'Hello ' + request.args['name']
-            api_address='https://api.openweathermap.org/data/2.5/weather?appid=3e90723e72e3e77055f8c10dccb120f8&q='
-            # city = input('City Name :')
-            city = cityname
-            url = api_address + city
-            json_data = requests.get(url).json()
-            formatted_data = json_data['main']['temp']
-            print(formatted_data)
-            return formatted_data
-            #return make_response(jsonify({'fulfillmentText': "🙂Okay..!Follow the 3 more steps ..Enter Your Name Please ?"}))
+           
+            return make_response(jsonify({'fulfillmentText': "🙂Okay..!Follow the 3 more steps ..Enter Your Name Please ?"}))
 
     elif action == "uname":
            #if 'username' in request.args:

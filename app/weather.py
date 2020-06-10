2cfee07ac323e58341ba623f8c850e7e433b0bb1
imran <imranlatur33@gmail.com>
2020-06-10 19:01:22 +0530

    make it better

--- app/weather.py
+++ app/weather.py
@@ -14,3 +14,19 @@ json_data = requests.get(url).json()
 print("json_data :",json_data)
 formatted_data = json_data['main']['temp']
 print("you city temp is :",formatted_data)
+
+elif action == "city":
+           #if 'city'  request.args:
+            #return 'Hello ' + request.args['city']
+
+            api_address='https://api.openweathermap.org/data/2.5/weather?appid=3e90723e72e3e77055f8c10dccb120f8&q='
+            city = cityname
+            print("city :",city)
+            url = api_address + city
+            print("url :",url)
+            json_data = requests.get(url).json()
+            print("json_data :",json_data)
+            formatted_data = json_data['main']['temp']
+            print("you city temp is :",formatted_data)
+            return make_response(jsonify({'fulfillmentText': formatted_data}))
+
